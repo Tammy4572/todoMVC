ebe044d1655e029b46af5f06f3a57f1c1af5d01c
Tammy Castro <tammycastro@gmail.com>
2017-08-16 22:04:32 -0400

    corrected models/todos rendering

--- app.js
+++ app.js
@@ -3,79 +3,63 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-const Todos = require('./models/todos');
+
+const Todos = require('./models/todos.js');
 const bodyParser = require('body-parser');
 const router = express.Router();
 
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/todoMVC');
+mongoose.connect('mongodb://localhost:27017/todos');
 
-app.use('/static', express.static('static'));
 app.use(bodyParser.json());
+app.use('/static', express.static('static'));
 
-app.get('/',  function(request, response) {
+app.get('/', function(request, response) {
     response.sendFile(__dirname + "/static/index.html");
 });
 
 //returns a JSON array of todo items
 app.get('/api/todos', async (request, response) => {
-    var results = await Todos.find();
-    if(!results) {
-        response.status(404);
-        return response.end();
-    }
-    response.json(results);
+    var todos = await Todos.find();
+    response.json(todos);
 });
 
 //get a specific todo item
 app.get('/api/todos/:id', async (request, response) => {
-    var id = request.params.id;
-    var todo = await Todos.find({ _id: id});
 
-    if (!todo) {
-        response.status(404);
-        return response.end();
-    }
-    return response.json(todo);
+    let todos = await Todos.findOne({ id: parseInt(request.params.id)});
+    response.json(todos);
 });
 
 //return a saved todo JSON item
 app.post('/api/todos', async(request, response) => {
-    var newTodo = new Todos({
-        title: request.body.title,
-        order: await Todos.count() +1,
-        completed: false
-    })
-    newTodo.save();
-    response.status(200);
-    return response.json(newTodo);
+     var title = request.body;
+     let todos = new Todos(request.body)
+          todos.save(function(err, newTodo){
+               response.json(newTodo);
+          })
 });
 
 //update a todo item - returns the modified todo item
 app.put('/api/todos/:id', async (request, response) => {
-    var id = request.params.id;
-    let title = request.body.title;
-    await Todos.findOneAndUpdate({ _id: id },
-        {
-            $set: {
-                title: title,
-                completed: completed
-            }
-        }
-    );
-    let updated = await Todos.find({ _id: id })
-
-    return response.json(updated);
+     await Todos.updateOne(
+          {id: parseInt(request.params.id)},
+          {title: request.body.title,
+          order: request.body.order,
+          completed: request.body.completed
+          }
+     );
+     var todo = await Todos.find({id: parseInt(request.params.id)});
+     request.json(todos);
 });
 
 //delete a todo item - returns the todo item that was deleted
 app.delete('/api/todos/:id', async (request, response) => {
-    var id = request.params.id;
-    var todo = await Todos.findOne({ _id: id});
-    var deleted = await Todos.deleteOne({ _id: id});
-    return response.json(todo);
+    let todos = await Todos.findOne({ id: parseInt(request.params.id)});
+    await Todos.deleteOne({ id: parseInt(request.params.id)});
+    response.json(todo);
 });
 
 app.listen(3000, function () {
---
+++ models/todos.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+    // Todos = mongoose.model('Todos');
+
+const todoSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    order: { type: Number, required: true },
+    completed: { type: Boolean, default: false }
+});
+
+const Todos = mongoose.model('Todos', todoSchema);
+
+module.exports = Todos;
